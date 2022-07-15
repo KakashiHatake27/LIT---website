9ad17b0f2b623a45be60b506d6d6127da0d0ec29
unknown <trialacc.akshdeep@gmail.com>
2022-07-15 21:23:16 +0200

    Added more products

--- prod-app.js
+++ prod-app.js
@@ -49,3 +49,51 @@ document.getElementById('num').onclick = function() {
         alert('Whatsapp number has been copied')
     })
 }
+
+
+
+// Hamburger Menu
+
+// var logo = document.getElementsByClassName("logo")[0];
+// logo.style.display = "none";
+// console.log(logo);
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+                // logo.style.display = "block";
+            } else {
+
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`;
+                // logo.style.display = "none";
+            }
+        });
+        burger.classList.toggle('toggle');
+    });
+
+
+
+}
+
+navSlide();
+
+
+
+
+/*EXPAND IMAGE*/
+
+function expImage(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
